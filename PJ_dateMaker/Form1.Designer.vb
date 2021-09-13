1b608bd4738c68310e7c1090a8f1125a5c7abc1c
38Koo <captain.marvel.mk@gmail.com>
2021-09-13 23:47:39 +0900

    initial commit

---
+++ PJ_dateMaker/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgv1 = New System.Windows.Forms.DataGridView()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblDate = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgv1
+        '
+        Me.dgv1.AllowUserToAddRows = False
+        Me.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv1.Location = New System.Drawing.Point(26, 210)
+        Me.dgv1.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.dgv1.Name = "dgv1"
+        Me.dgv1.RowHeadersVisible = False
+        Me.dgv1.RowHeadersWidth = 82
+        Me.dgv1.RowTemplate.Height = 21
+        Me.dgv1.ScrollBars = System.Windows.Forms.ScrollBars.None
+        Me.dgv1.Size = New System.Drawing.Size(1138, 790)
+        Me.dgv1.TabIndex = 1000
+        Me.dgv1.TabStop = False
+        '
+        'txtDate
+        '
+        Me.txtDate.Font = New System.Drawing.Font("MS UI Gothic", 18.0!)
+        Me.txtDate.ForeColor = System.Drawing.Color.Black
+        Me.txtDate.Location = New System.Drawing.Point(1437, 138)
+        Me.txtDate.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.txtDate.Multiline = True
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(832, 752)
+        Me.txtDate.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1437, 906)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(836, 86)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "コピー"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1437, 1004)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(836, 86)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "リセット"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'lblDate
+        '
+        Me.lblDate.AutoSize = True
+        Me.lblDate.Font = New System.Drawing.Font("MS UI Gothic", 20.0!)
+        Me.lblDate.Location = New System.Drawing.Point(462, 104)
+        Me.lblDate.Margin = New System.Windows.Forms.Padding(7, 0, 7, 0)
+        Me.lblDate.Name = "lblDate"
+        Me.lblDate.Size = New System.Drawing.Size(212, 54)
+        Me.lblDate.TabIndex = 4
+        Me.lblDate.Text = "2020/01"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(319, 104)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(72, 54)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(758, 104)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(72, 54)
+        Me.Button4.TabIndex = 1001
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(913, 104)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(7, 6, 7, 6)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(72, 54)
+        Me.Button5.TabIndex = 1002
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(2740, 1783)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.lblDate)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.dgv1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.Name = "Form1"
+        Me.Text = "以下の日程でご都合いかがでしょうか？"
+        CType(Me.dgv1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgv1 As DataGridView
+    Friend WithEvents txtDate As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents lblDate As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+End Class
---
+++ PJ_dateMaker/Form1.vb
@@ -0,0 +1,297 @@
+﻿Public Class Form1
+    Const word As String = "以下の日程でご都合いかがでしょうか？" + vbCrLf
+    Dim Year As Integer
+    Dim month As Integer
+    Dim day As Integer
+    Dim lastMonth As Integer　'今月最後の日付を格納
+    Dim preLastMonth As Integer '先月最後の日付を格納
+
+    ''デフォルトのセルスタイル
+    'Private defaultCellStyle As DataGridViewCellStyle
+    ''マウスポインタの下にあるセルのセルスタイル
+    'Private mouseCellStyle As DataGridViewCellStyle
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            Dim dt As DateTime = DateTime.Now
+            txtDate.Text = word
+            txtDate.SelectionStart = word.Length + 1
+
+            '今日日付の各要素取得
+            Year = dt.Year
+            month = dt.Month
+            day = dt.Day
+
+            '今日の年月表示
+            lblDate.Text = Year.ToString + "/" + month.ToString
+
+            'カレンダー作成
+            getCalender()
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+#Region "関数"
+
+#Region "カレンダー作成"
+    Private Sub getCalender()
+        Try
+            Dim num As Integer = 1
+            Dim memdt As New Date
+            Dim row As Integer = 0
+            Dim column As Integer
+            Dim num2 As Integer = 1
+            Dim preMonth As Integer
+            'Dim style As New DataGridViewCellStyle
+
+            'ヘッダテキスト配列
+            Dim week As String() = {"日", "月", "火", "水", "木", "金", "土"}
+            'ヘッダ名配列
+            Dim weekNm As String() = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+            'dgv1にヘッダ追加、幅決定、Alignment指定
+            For i As Integer = 0 To week.Length - 1
+                dgv1.Columns.Add(weekNm(i), week(i))
+                dgv1.Columns(i).Width = 75
+                dgv1.Columns(i).HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            Next
+
+            '今はヘッダしかないのでとりあえず一行追加
+            dgv1.Rows.Add()
+
+            '今月最後の日付を取得
+            lastMonth = DecLastMonth(month)
+
+            '表示されている年月の一日を取得
+            memdt = CDate(lblDate.Text + "/1")
+
+            'Debug.WriteLine(Array.IndexOf(week, memdt.ToString("ddd")))
+
+            '表示されている年月の一日が何曜日かを取得（カレンダーの始まる曜日を取得
+            column = Array.IndexOf(week, memdt.ToString("ddd"))
+
+            '先月最後の日付も取得する
+            If column <> 0 Then
+                preMonth = month
+                If preMonth = 1 Then
+                    preMonth = 12
+                Else
+                    preMonth -= 1
+                End If
+                preLastMonth = DecLastMonth(preMonth)
+                'カレンダーが月曜日以降から始まる場合、先月の最後の方の日付も表示する
+                For i As Integer = column - 1 To 0 Step -1
+                    dgv1.Item(i, row).Value = preLastMonth
+                    preLastMonth -= 1
+                Next
+            End If
+
+            '表示されている年月のカレンダー作成
+            For i As Integer = 1 To lastMonth
+                dgv1.Item(column, row).Value = i
+
+                If i <> lastMonth Then
+                    If column = 6 Then
+                        dgv1.Rows.Add()
+                        column = 0
+                        row += 1
+                    Else
+                        column += 1
+                    End If
+                End If
+            Next
+
+            'カレンダーが土曜日よりも前で終わった場合、来月の最初の方の日付も表示する
+            If column <> 6 Then
+                For i As Integer = column + 1 To 6
+                    dgv1.Item(i, dgv1.Rows.Count - 1).Value = num2
+                    num2 += 1
+                Next
+            End If
+
+            '行の高さ設定
+            For i As Integer = 0 To dgv1.Rows.Count - 1
+                dgv1.Rows(i).Height = 75
+            Next
+
+            'dgv1の高さを設定
+            dgv1.Height = 75 * dgv1.Rows.Count + 20
+
+            '土曜日、日曜日に色付け、文字サイズ変更
+            dgv1.Columns(0).DefaultCellStyle.ForeColor = Color.Red
+            dgv1.Columns(6).DefaultCellStyle.ForeColor = Color.Blue
+            dgv1.DefaultCellStyle.Font = New Font(“ＭＳ ゴシック”, 20)
+
+            '先月分と来月分の文字色を薄くする
+            For i As Integer = 0 To 6
+                If dgv1.Item(i, 0).Value > 10 Then
+                    Select Case i
+                        Case 0
+                            dgv1(i, 0).Style.ForeColor = Color.FromArgb(255, 128, 128)
+                        Case Else
+                            dgv1(i, 0).Style.ForeColor = Color.Silver
+                    End Select
+                End If
+
+                If dgv1.Item(i, dgv1.Rows.Count - 1).Value < 10 Then
+                    Select Case i
+                        Case 6
+                            dgv1(i, dgv1.Rows.Count - 1).Style.ForeColor = Color.FromArgb(128, 128, 255)
+                        Case Else
+                            dgv1(i, dgv1.Rows.Count - 1).Style.ForeColor = Color.Silver
+                    End Select
+                End If
+            Next
+
+            'ロード時のフォーカスを今日日付に充てたい
+            'For i As Integer = 0 To dgv1.Rows.Count - 1
+            '    For j As Integer = 0 To 6
+            '        If dgv1.Item(j, i).Value = day Then
+            '            If day > 20 AndAlso i <> 0 Then
+            '                dgv1.CurrentCell = dgv1(j, i)
+            '            End If
+            '        End If
+            '    Next
+            'Next
+
+            'Debug.WriteLine(dgv1.ColumnHeadersHeight)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+#End Region
+
+#End Region
+
+    Private Function DecLastMonth(month As Integer) As Integer
+        Try
+            '月によって最後の日付を取得
+            Dim lastMonthDate As Integer
+            Select Case month
+                Case 1, 3, 5, 7, 8, 10, 12
+                    lastMonthDate = 31
+                Case 4, 6, 9, 11
+                    lastMonthDate = 30
+                Case 2
+                    'うるう年も判定
+                    If DateTime.IsLeapYear(Year) = True Then
+                        lastMonthDate = 29
+                    Else
+                        lastMonthDate = 28
+                    End If
+            End Select
+
+            Return lastMonthDate
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            txtDate.Clear()
+            txtDate.Text = word
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Clipboard.SetText(txtDate.Text)
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgv1.CellClick
+        Try
+            Dim strText As String = txtDate.Text
+            Dim dateText As String
+            dateText = month.ToString + "月" + dgv1(e.ColumnIndex, e.RowIndex).Value.ToString + "日" _
+                + "(" + dgv1.Columns(e.ColumnIndex).HeaderText + ")"
+
+            strText += dateText
+
+            txtDate.Text = strText
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Try
+            'dgv1をクリア
+            dgv1.DataSource = Nothing
+            dgv1.Rows.Clear()
+
+
+            '先月の月を取得
+            If month = 1 Then
+                month = 12
+                Year -= 1
+            Else
+                month -= 1
+            End If
+
+            '先月の年月表示
+            lblDate.Text = Year.ToString + "/" + month.ToString
+
+            'カレンダー作成
+            getCalender()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Try
+            'dgv1をクリア
+            dgv1.DataSource = Nothing
+            dgv1.Rows.Clear()
+
+
+            '来月の月を取得
+            If month = 12 Then
+                month = 1
+                Year += 1
+            Else
+                month += 1
+            End If
+
+            '来月の年月表示
+            lblDate.Text = Year.ToString + "/" + month.ToString
+
+            'カレンダー作成
+            getCalender()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Try
+            'dgv1をクリア
+            dgv1.DataSource = Nothing
+            dgv1.Rows.Clear()
+
+            Dim dt As DateTime = DateTime.Now
+
+            txtDate.Focus()
+
+            '今日日付の各要素取得
+            Year = dt.Year
+            month = dt.Month
+            day = dt.Day
+
+            '今日の年月表示
+            lblDate.Text = Year.ToString + "/" + month.ToString
+
+            'カレンダー作成
+            getCalender()
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Sub
+End Class
